4cd287c50ec19e1c57bcf6916e26939f91e45e32
ahri <nurym.asqar@gmail.com>
2023-10-10 15:40:21 +0600

    Added Documentation model package

--- src/design/model/PC.java
+++ src/design/model/PC.java
@@ -6,22 +6,18 @@ public class PC extends Computer {
     private String cpu;
 
     /**
-     *
+     * Constructor to initialize PC specifications.
      * @param ram - size of PC RAM
      * @param hdd - capacity of PC HDD
      * @param cpu - frequency of PC CPU
      */
     public PC(String ram, String hdd, String cpu){
-
-        this.ram=ram;
-        this.hdd=hdd;
-        this.cpu=cpu;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.cpu = cpu;
     }
 
-    /**
-     * Getting max RAM amount, and other specifications
-     * @return RAM info
-     */
+    // Overrides the abstract methods to provide PC specifications
     @Override
     public String getRAM() { return this.ram; }
     @Override
--- src/design/model/Server.java
+++ src/design/model/Server.java
@@ -6,16 +6,18 @@ public class Server extends Computer {
     private String cpu;
 
     /**
-     *
+     * Constructor to initialize Server specifications.
      * @param ram - size of server RAM
      * @param hdd - capacity of server HDD
      * @param cpu - frequency of server CPU
      */
     public Server(String ram, String hdd, String cpu){
-        this.ram=ram;
-        this.hdd=hdd;
-        this.cpu=cpu;
+        this.ram = ram;
+        this.hdd = hdd;
+        this.cpu = cpu;
     }
+
+    // Overrides the abstract methods to provide Server specifications
     @Override
     public String getRAM() { return this.ram; }
     @Override
